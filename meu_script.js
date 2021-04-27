7ee1df2ee6bd0137264586040870896c3da297e4
ereseda <82920295+EliReseda@users.noreply.github.com>
2021-04-27 10:57:22 -0300

    Estudo de variáveis
    Estudo de variáveis

--- meu_script.js
+++ meu_script.js
@@ -1,1 +1,7 @@
-alert('Olá, Seja bem vindo ao curso de JacaScript!!!')
+//alert('Olá, Seja bem vindo ao curso de JacaScript!!!')
+document.getElementById('nome').value='Oi'
+
+/*
+		Selecionar um elemento no DOM
+		atualizar o valor desse elemento com uma string
+*/
